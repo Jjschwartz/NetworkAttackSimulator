0eba61fa3f617beabf5e82867ced8dd018bfef89
Jonathon <Jonathon.schwartz12@gmail.com>
2021-03-15 14:41:15 +1100

    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extras['all'] = [item for group in extras.values() for item in group]
 
 setup(
     name='nasim',
-    version='0.7.5',
+    version='0.8.0',
     url="https://networkattacksimulator.readthedocs.io",
     description="A simple and fast simulator for remote network pen-testing",
     long_description=open('README.rst').read(),
